5c278c95a6ae86572d896be3b8b92edf9f0c92ea
Brian Hall <bhall7@hawaii.edu>
2013-09-23 12:58:53 -1000

    ready to go

--- all_tests.py
+++ all_tests.py
@@ -11,10 +11,29 @@ class TestPhredHelper(unittest.TestCase):
         self.assertEqual(64, helper2.offset)
 
     def test_char_to_int(self):
+        # Sanger
         helper1 = PhredHelper()
         self.assertEqual(0, helper1.char_to_int('!'))
+        self.assertEqual(10, helper1.char_to_int('+'))
+        self.assertEqual(27, helper1.char_to_int('<'))
+        # Illumina
+        helper2 = PhredHelper(64)
+        self.assertEqual(0, helper2.char_to_int('@'))
+        self.assertEqual(10, helper2.char_to_int('J'))
+        self.assertEqual(27, helper2.char_to_int('['))
 
-
+    def test_int_to_char(self):
+        #Sanger
+        helper1 = PhredHelper()
+        self.assertEqual('I', helper1.int_to_char(40))
+        self.assertEqual('?', helper1.int_to_char(30))
+        self.assertEqual('5', helper1.int_to_char(20))
+        # Illumina
+        helper2 = PhredHelper(64)
+        self.assertEqual('h', helper2.int_to_char(40))
+        self.assertEqual('^', helper2.int_to_char(30))
+        self.assertEqual('T', helper2.int_to_char(20))
+        
 
 
 
--- phred.py
+++ phred.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 
-sanger_dict = {'!': 0}
-
-illumina_dict = {'@': 0}
-
 class PhredHelper:
     def __init__(self, offset=33):
-        self.offset = offset
-        if self.offset == 33:
-            self.dictionary = sanger_dict
-        elif self.offset == 64:
-            self.dictionary = illumina_dict
-        else:
-            sys.stderr.write("Invalid offset value! Using default (Sanger 'Q+33')")
+        if offset != 33 and offset != 64:
+            sys.err.write("PhredHelper: invalid offset specified. Using default (Sanger 'Q+33')")
             self.offset = 33
-            self.dictionary = sanger_dict
+        else:
+            self.offset = offset
 
     def char_to_int(self, char):
-        return(self.dictionary['!'])
+        raw_val = ord(char)
+        phred_val = raw_val - self.offset
+        return phred_val
+
+    def int_to_char(self, num):
+        return chr(num + self.offset)
